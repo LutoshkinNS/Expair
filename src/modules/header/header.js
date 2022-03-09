baebef75190ccb59b82c68d47d2a5d564d90b4c3
Nikita <n28280202@gmail.com>
2022-03-09 23:12:17 +0300

    Убран вертикальный скролл при открытом бургер меню

--- src/modules/header/header.js
+++ src/modules/header/header.js
@@ -12,11 +12,19 @@ const header = () => {
 
     burgerBtn.onclick = () => {
       burgerMenu.classList.add('burger-menu_show');
+      burgerMenu.style.opacity = '1';
+      document.body.style.overflow = 'hidden';
+      document.body.style.position = 'fixed';
     };
 
     document.onclick = (e) => {
       if (!e.target.matches('#burgerBtn') && (e.target.matches('#burgerMenuClose') || !e.target.closest('#burgerMenu'))) {
         burgerMenu.classList.remove('burger-menu_show');
+        document.body.style.overflow = 'initial';
+        document.body.style.position = 'initial';
+        setTimeout(() => {
+          burgerMenu.style.opacity = '0';
+        }, 500);
       }
     };
   }
